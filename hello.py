e92504ef3e09715a1c782b1a2835029a7a3f4350
Seongjaehun <tjdwognszz@hanyang.ac.kr>
2022-05-06 13:21:29 +0900

    깃을 이용한 실습 과제
    조건에 따른 값 출력하기

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 # 과제: 1부터 10까지 반복하는 과정에서 3의 배수일 경우, year를 출력하시오.
 # 추가 과제 : 5의 배수일 때, dream 출력
+# 추가 과제2 : 15의 배수일 때, yeardream
 # 나머지 모든 경우는 숫자 그대로 출력
 
 for i in range(1,21):
@@ -7,6 +8,8 @@ for i in range(1,21):
         print('year')
     elif i % 5 == 0:
         print('dream')
+    elif (i % 3 == 0) and (i % 5 == 0):
+        print('yeardream')
     else:
         print(i)
 
