8df4ef3b4bcf7b6490eee152eabc02fa8d471772
Seongjaehun <tjdwognszz@hanyang.ac.kr>
2022-05-06 13:33:46 +0900

    깃을 이용한 실습과제
    배수 조건에 따른 값 출력하기

--- hello.py
+++ hello.py
@@ -4,12 +4,12 @@
 # 나머지 모든 경우는 숫자 그대로 출력
 
 for i in range(1,21):
-    if i % 3 == 0:
+    if i % 15 == 0:
+        print('yeardream')
+    elif i % 3 == 0:
         print('year')
     elif i % 5 == 0:
         print('dream')
-    elif (i % 3 == 0) and (i % 5 == 0):
-        print('yeardream')
     else:
         print(i)
 
